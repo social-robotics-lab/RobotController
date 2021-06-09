036a97095dabc8a0611262c0c1a31b00aa13d910
iio <iio@sce.iit.tsukuba.ac.jp>
2021-06-09 11:06:58 +0900

    Removed a library for AMQ.

--- src/main/App.java
+++ src/main/App.java
@@ -3,12 +3,10 @@ package main;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import javax.jms.JMSException;
-
 
 public class App {
 
-	public static void main(String[] args) throws JMSException {
+	public static void main(String[] args) {
 		ExecutorService service = Executors.newFixedThreadPool(3);
 		service.execute(new SpeechServer(Params.speechPort));
 		service.execute(new PoseServer(Params.posePort));
